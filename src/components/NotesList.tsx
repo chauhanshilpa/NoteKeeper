da6dd31f7ad17e0d528bd94e3fc3bc2aedf4e872
Shilpa Chauhan <101130846+chauhanshilpa@users.noreply.github.com>
2024-06-15 00:21:20 +0530

    Remove non-usable code

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { Note } from "../utils/classModels";
 import NoteCard from "./NoteCard";
 
@@ -8,17 +7,6 @@ interface Props {
 }
 
 const NotesList = ({ currentNotesList, setNotesList }: Props) => {
-  // useEffect(() => {
-  //   const grids = document.querySelectorAll(".grid");
-  //   grids.forEach((grid) => {
-  //     const items = Array.from(grid.children);
-  //     items.forEach((item) => {
-  //       (item as HTMLElement).style.gridRowEnd = `span ${Math.ceil(
-  //         (item as HTMLElement).clientHeight / 10
-  //       )}`;
-  //     });
-  //   });
-  // }, [currentNotesList]);
 
   const pinnedNotesList = currentNotesList.filter(
     (note) => note.isPinned === true
@@ -30,14 +18,12 @@ const NotesList = ({ currentNotesList, setNotesList }: Props) => {
   return (
     <div className="w-[80%] m-auto mt-14">
       {pinnedNotesList.length > 0 && (
-        <div className="w-[80%] columns-1 sm:columns-2 md:columns-3 xl:columns-4 m-auto">
+        <div className="xs:w-[60%] sm:w-[100%] columns-1 sm:columns-2 md:columns-3 2xl:columns-4 m-auto mb-10">
           {pinnedNotesList.map((note) => (
             <NoteCard key={note.id} note={note} setNotesList={setNotesList} />
           ))}
         </div>
       )}
-      {/* flex flex-wrap justify-center sm:grid sm:grid-cols-3 gap-5 xl:grid-cols-4
-      m-auto */}
       {unpinnedNotesList.length > 0 && (
         <div className="xs:w-[60%] sm:w-[100%] columns-1 sm:columns-2 md:columns-3 2xl:columns-4 m-auto">
           {unpinnedNotesList.map((note) => (
