3d43b0df9e5428fec06ea77927580a3cabbe6e27
Shilpa Chauhan <chauhanshilpa602@gmail.com>
2024-06-15 19:11:33 +0530

    Add backend

---
+++ backend/index.ts
@@ -0,0 +1,62 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { v4 as uuidv4 } from "uuid";
+import { Note } from "../frontend/src/utils/classModels";
+
+const port: Number = 4001;
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+let notesList: Note[] = [];
+
+app.post("/create_note", async (req, res) => {
+  const { inputNoteTitle, inputNoteTagline, inputNoteBody } = req.body;
+  let noteId = uuidv4();
+  let newNote = new Note(
+    noteId,
+    inputNoteTitle,
+    inputNoteTagline,
+    inputNoteBody,
+    false,
+    false,
+    new Date().toLocaleString()
+  );
+  notesList.push(newNote);
+  res.send();
+});
+
+app.get("/notes_list", async (req, res) => {
+  const newNotesList = [...notesList];
+  res.send({ newNotesList });
+});
+
+app.patch("/update_note", async (req, res) => {
+  const { noteId, newTitle, newTagline, newBody } = req.body;
+  const toUpdateNoteIndex = notesList.findIndex((note) => note.id === noteId);
+  notesList[toUpdateNoteIndex].title = newTitle;
+  notesList[toUpdateNoteIndex].tagline = newTagline;
+  notesList[toUpdateNoteIndex].body = newBody;
+  res.send();
+});
+
+app.patch("/update_pin_status", async (req, res) => {
+  const { noteId, isPinned } = req.body;
+  let newNotesList = [...notesList];
+  const noteToChangePinnedState = newNotesList.findIndex(
+    (note) => note.id === noteId
+  );
+  newNotesList[noteToChangePinnedState].isPinned = isPinned;
+  res.send();
+});
+
+app.delete("/delete_note", async (req, res) => {
+  const { noteId } = req.query;
+  const noteToDeleteIndex = notesList.findIndex((note) => note.id === noteId);
+  // In database you can set isDeleted to true
+  notesList.splice(noteToDeleteIndex, 1);
+  res.send();
+});
+
+app.listen(port, () => console.log(`App is listening on port ${port}`));
---
+++ frontend/src/components/AddNoteCard.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { IoIosAdd } from "react-icons/io";
+import { CiEraser } from "react-icons/ci";
+import { createNote, getNotesList } from "../utils/api";
+import { Note } from "../utils/classModels";
+import { Tooltip } from "react-tooltip";
+
+interface Props {
+  setIsAddNoteClicked: React.Dispatch<React.SetStateAction<boolean>>;
+  setNotesList: React.Dispatch<React.SetStateAction<Note[]>>;
+}
+
+const AddNoteCard = ({ setIsAddNoteClicked, setNotesList }: Props) => {
+  const [inputNoteTitle, setInputNoteTitle] = useState<string>("");
+  const [inputNoteTagline, setInputNoteTagline] = useState<string>("");
+  const [inputNoteBody, setInputNoteBody] = useState<string>("");
+
+  async function handleAddNote(
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) {
+    event.preventDefault();
+    await createNote(inputNoteTitle, inputNoteTagline, inputNoteBody);
+    const response = await getNotesList();
+    setNotesList(response.data.newNotesList);
+    setIsAddNoteClicked(false);
+  }
+
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col fixed justify-center bg-gray-300 p-5 rounded-xl m-auto w-[30%]">
+        <img
+          src="https://note-keeper.s3.eu-north-1.amazonaws.com/note-keeper-icons/cancel.png"
+          alt="cancel"
+          className="h-5 w-5 cursor-pointer self-end"
+          onClick={() => setIsAddNoteClicked(false)}
+          data-tooltip-id="cancel"
+          data-tooltip-content="Cancel"
+        />
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="text"
+            name="title"
+            className="py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 focus:outline-none focus:ring-0 peer"
+            placeholder="title"
+            required
+            value={inputNoteTitle}
+            onChange={(event) => setInputNoteTitle(event?.target.value)}
+          />
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="text"
+            name="tagline"
+            className="py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 focus:outline-none focus:ring-0 peer"
+            placeholder="tagline"
+            value={inputNoteTagline}
+            onChange={(event) => setInputNoteTagline(event?.target.value)}
+          />
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="text"
+            name="body"
+            className="py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 focus:outline-none focus:ring-0 peer"
+            placeholder="body"
+            required
+            value={inputNoteBody}
+            onChange={(event) => setInputNoteBody(event?.target.value)}
+          />
+        </div>
+        <div className="flex">
+          <button
+            type="button"
+            className="flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            onClick={handleAddNote}
+          >
+            <span>
+              <IoIosAdd className="text-xl" />
+            </span>
+            <span>Add</span>
+          </button>
+          <button
+            type="button"
+            className="flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            onClick={() => {
+              setInputNoteTitle("");
+              setInputNoteTagline("");
+              setInputNoteBody("");
+            }}
+          >
+            <span>
+              <CiEraser className="text-xl mr-1" />
+            </span>
+            <span>Clear field</span>
+          </button>
+        </div>
+      </div>
+      <Tooltip id="cancel" className="tooltip" />
+    </div>
+  );
+};
+
+export default AddNoteCard;
