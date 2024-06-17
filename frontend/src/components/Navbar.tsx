44c872eed376dc745476365da2398e240921253e
Shilpa Chauhan <101130846+chauhanshilpa@users.noreply.github.com>
2024-06-17 11:48:54 +0530

    Add a background image on navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-
 interface Props {
   setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
 }
@@ -8,7 +7,16 @@ const Navbar = ({ setCurrentPage }: Props) => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex top-0 sticky items-center p-5 shadow-md h-14 bg-gray-50">
+    <div
+      className="flex top-0 sticky items-center p-5 shadow-md shadow-[#4A7B43] h-14 bg-gray-50"
+      style={{
+        backgroundImage:
+          "url(https://note-keeper.s3.eu-north-1.amazonaws.com/note-keeper-icons/navbar-bg.jpg)",
+        backgroundSize: "cover",
+        backgroundRepeat: "repeat",
+        backgroundPosition: "right",
+      }}
+    >
       <div
         className="flex items-center gap-2 italic text-lg"
         onClick={() => {
@@ -21,7 +29,7 @@ const Navbar = ({ setCurrentPage }: Props) => {
           alt="note-keeper-icon"
           className="h-10 w-10 cursor-pointer hover:scale-105"
         />
-        <span className="font-bold">Notes Keeper</span>
+        <span className="font-bold text-gray-50">Notes Keeper</span>
       </div>
     </div>
   );
